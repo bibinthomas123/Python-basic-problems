aaa061e311ef6c3655221f19279beda75b51cb57
=bibin thomas <bibinthomas951@gmail.com>
2022-07-21 22:59:04 +0530

    Radian to degree

--- radian.py
+++ radian.py
@@ -9,6 +9,6 @@ radian = int(input("Enter the radian value:")) #takes the input from the user
 
 degree = radian * 180/pi  #formula 
  
-print("the degree of {0} radian is :{1:.2f}".format(radian,degree)) # the format is used to insert a value into the sting instead of concatination 
+print("the degree of {0} radian is : {1:.2f}".format(radian,degree)) # the format is used to insert a value into the sting instead of concatination 
 # where {0} indicate the first variable inside the .format and {1} for the second var in the .format 
 #:.2f is used to truncate into 2 decimal places ex 3.14 instead of  3.1456896568523
